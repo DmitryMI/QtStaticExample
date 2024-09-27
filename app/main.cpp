9dbd410b088cdf4a616bd6c1c3b09281854b0fd6
Dmitriy Monakhov <dimamon6032403@gmail.com>
2024-09-27 10:56:21 +0200

    Initial commit

---
+++ app/main.cpp
@@ -0,0 +1,15 @@
+﻿// QtStaticExample.cpp : Defines the entry point for the application.
+//
+
+#include "MainWindow.h"
+#include <QtWidgets/QApplication>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
